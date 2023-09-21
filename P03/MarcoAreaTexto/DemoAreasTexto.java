b0daef2260c1fc082b0f34b767ccf64633aa72d0
CristianFu <crispinsar987@gmail.com>
2023-09-21 15:12:33 -0600

    Practica 3

---
+++ P03/MarcoAreaTexto/DemoAreasTexto.java
@@ -0,0 +1,13 @@
+package MarcoAreaTexto;
+// Fig. 12.48: DemoAreaTexto.java
+// Prueba de MarcoAreaTexto. 
+import javax.swing.JFrame;
+
+public class DemoAreasTexto {
+    public static void main(String[] args) { 
+        MarcoAreaTexto marcoAreaTexto = new MarcoAreaTexto(); 
+        marcoAreaTexto.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marcoAreaTexto.setSize(425, 200);
+        marcoAreaTexto.setVisible(true);
+    } 
+} // fin de la clase DemoAreaTexto
